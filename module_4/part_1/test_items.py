b9e5d10b6fade52ba48f66d496e72271a63e2c4a
shakhnovapolina <polina.shakhnova@blitzteam.com>
2021-06-08 14:31:41 +0300

    fix hardcode locators part_1, module 4

--- module_4/part_1/test_items.py
+++ module_4/part_1/test_items.py
@@ -1,14 +1,15 @@
 link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 lang = {"ru": "Добавить в корзину", "en": "Add to basket", "es": "Añadir al carrito", "fr": "Ajouter au panier"}
-
+button_add_backet_locator = "button.btn-add-to-basket"
+droplist_language_option_locator = '//option[@selected="selected"]'
 
 def test_check_language(browser):
     # Arrange
     browser.get(link)
 
     # Act
-    button_add_backet = browser.find_element_by_css_selector("button.btn-add-to-basket")
-    droplist_language_option = browser.find_element_by_xpath('//option[@selected="selected"]')
+    button_add_backet = browser.find_element_by_css_selector(button_add_backet_locator)
+    droplist_language_option = browser.find_element_by_xpath(droplist_language_option_locator)
     selected_language = droplist_language_option.get_attribute("value")
 
     # Assert
