eab0d6c411066120cdff08cabe608f4f188f717f
shakhnovapolina <polina.shakhnova@blitzteam.com>
2021-06-15 12:24:07 +0300

    add marks to the offer7

--- module_5/test_product_page.py
+++ module_5/test_product_page.py
@@ -8,7 +8,8 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
 class TestProductPage:
 
     @pytest.mark.parametrize('promo_offer',
-                             ["offer0", "offer1", "offer2", "offer3", "offer4", "offer5", "offer6", "offer7", "offer8",
+                             ["offer0", "offer1", "offer2", "offer3", "offer4", "offer5", "offer6",
+                              pytest.param("offer7", marks=pytest.mark.xfail), "offer8",
                               "offer9"])
     def test_guest_can_add_product_to_basket(self, browser, promo_offer):
         page = ProductPage(browser,
